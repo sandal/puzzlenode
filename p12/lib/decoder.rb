1d2c704be64bf669438f1a5c350a89be06d691b5
Gregory Brown <gregory.t.brown@gmail.com>
2014-03-07 20:59:43 -0500

    Remove some manual iteration

--- p12/lib/decoder.rb
+++ p12/lib/decoder.rb
@@ -1,27 +1,19 @@
 def possibleWords(word, shift, alpha)
-    key = alpha[shift..-1]+alpha[0..shift]
-    newWord = ''
-    
-    word.each_char do |char|
-      wordIndex = alpha.index(char)
-      newWord += key[wordIndex]
-    end
-    
-    return newWord
+  key = alpha[shift..-1]+alpha[0..shift]
+
+  word.chars.each_with_object('') do |char, new_word|
+    wordIndex = alpha.index(char)
+    newWord << key[wordIndex]
+  end
 end
-
+    
 def returnShiftedLetter(char, shift, alpha)
-    charIndex = alpha.index(char)
-    shiftDown = (charIndex.to_i - shift)
+  charIndex = alpha.index(char)
+  shiftDown = (charIndex.to_i - shift)
 
-    newLetter = alpha[shiftDown]
+  newLetter = alpha[shiftDown]
 end
 
 def returnVigKeys(word, alpha)
-    keys = []
-    word.split('').each do |i|
-      key = alpha.index(i)
-      keys.push(key)
-    end
-    return keys
+  word.chars.map  { |e| alpha.index(e) }
 end
